be8f60701fcee3f15b589be362d357be81ea90cd
Trip <chrisbitschi@hotmail.com>
2018-08-04 15:19:40 +0300

    Fixed a few things
    Fixed positioning of rename dialogue.
    Fixed crash on launch when there was a playlist saved.

---
+++ Form_General_Options.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form_General_Options
+
+    Private Sub cb_auto_aspect_CheckedChanged(sender As Object, e As EventArgs) Handles cb_auto_aspect.CheckedChanged
+        Form_Main.AutoAspect = Me.cb_auto_aspect.Checked
+    End Sub
+
+    Private Sub Form_General_Options_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.cb_auto_aspect.Checked = Form_Main.AutoAspect
+        cb_rating_filter.SelectedIndex = 2
+        Me.Location = New Point(Form_Main.Width / 2 + Form_Main.Location.X - (Me.Width / 2), Form_Main.Height / 2 + Form_Main.Location.Y - (Me.Height / 2))
+
+        cb_search.Items.Add("")
+        For Each cb In RenameForm.clb_cats.Items
+            If cb <> "" AndAlso cb <> "-" Then cb_search.Items.Add(cb)
+        Next
+        cb_search.SelectedIndex = 0
+    End Sub
+
+    Private Sub b_clearpl_Click(sender As Object, e As EventArgs) Handles b_clearpl.Click
+        For Each Player As AxAXVLC.AxVLCPlugin2 In Form_Main.Controls.OfType(Of AxAXVLC.AxVLCPlugin2)()
+            Player.playlist.stop()
+        Next
+        For a = 1 To 6
+            Form_Main.options_list(a).b_clear_playlist_Click(Nothing, Nothing)
+        Next
+    End Sub
+
+    Private Sub tb_dd_split_play_DragEnter(sender As Object, e As DragEventArgs) Handles tb_dd_split_play.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+            e.Effect = DragDropEffects.All
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub tb_dd_split_play_DragDrop(sender As Object, e As DragEventArgs) Handles tb_dd_split_play.DragDrop
+        Dim s() As String = e.Data.GetData(DataFormats.FileDrop, False)
+
+        best_filter = New List(Of String)
+        If cb_only_these.Checked = True Then
+            best_filter.Add(cb_rating_filter.Items(cb_rating_filter.SelectedIndex))
+        Else
+            For a = cb_rating_filter.SelectedIndex To 0 Step -1
+                best_filter.Add(cb_rating_filter.Items(a))
+            Next
+        End If
+
+        For Each fh In s
+            If System.IO.Directory.Exists(fh) Then
+                AddFolder(fh, Form_Main.options_list(1))
+            ElseIf System.IO.File.Exists(fh) Then
+                AddFile(fh, Form_Main.options_list(1))
+            End If
+        Next
+
+        Form_Main.options_list(1).PopulatePlayList()
+        Form_Main.options_list(1).b_random_Click(Nothing, Nothing)
+        Form_Main.options_list(1).b_split_list_Click(Nothing, Nothing)
+        For a = 1 To 6
+            If Form_Main.options_list(a).lb_playlist.Items.Count > 0 Then
+                Form_Main.options_list(a).lb_playlist.SelectedIndex = 0
+            End If
+        Next
+
+    End Sub
+
+    Private Sub AddFolder(fh As String, fo As FormOptions)
+        For Each fs In System.IO.Directory.EnumerateDirectories(fh)
+            AddFolder(fs, Form_Main.options_list(1))
+        Next
+        For Each fs In System.IO.Directory.EnumerateFiles(fh)
+            AddFile(fs, fo)
+        Next
+    End Sub
+
+    Private ratings() As String = {"vvgood", "vgood", "good", "ok", "poor", "Vvgood", "Vgood", "Good", "Ok", "Poor"}
+    Private best_filter As List(Of String)
+    Private cat_filter As String = ""
+
+    Private Sub AddFile(fh As String, fo As FormOptions)
+        Dim name As String = fh.Substring(fh.LastIndexOf("\") + 1)
+        Dim rating As String = ""
+        Dim valid_rating As Boolean = False
+        If name.IndexOf(" "c) > 0 Then
+            rating = name.Substring(0, name.IndexOf(" "c))
+            valid_rating = ratings.Contains(rating)
+        End If
+
+        If cb_search.SelectedItem <> "" AndAlso Not name.Split(" "c).Contains(cb_search.SelectedItem) Then
+            Exit Sub
+        End If
+
+        If cb_rating_filter.SelectedIndex = 6 Then
+            fo.AddFile(fh)
+        ElseIf Not valid_rating AndAlso cb_rating_filter.SelectedIndex = 5 Then
+            fo.AddFile(fh)
+        ElseIf valid_rating AndAlso best_filter.Contains(rating) Then
+            fo.AddFile(fh)
+        End If
+
+    End Sub
+
+    Private Sub b_double_mon_Click(sender As Object, e As EventArgs) Handles b_double_mon.Click
+        Form_Main.Location = New Point(-10, -30)
+        Form_Main.Size = New Size(2900, 1080)
+
+        Me.Close()
+    End Sub
+
+    Private Sub b_jump_Click(sender As Object, e As EventArgs) Handles b_jump.Click
+        For Each Player In Form_Main.TableLayoutPanel1.Controls.OfType(Of AxAXVLC.AxVLCPlugin2)()
+            If Player.input.length > 0 Then
+                Player.input.time = Player.input.time + Player.input.length / 5
+            End If
+        Next
+    End Sub
+End Class
---
+++ Rename.vb
@@ -0,0 +1,110 @@
+﻿Public Class RenameForm
+
+    Friend PlayerNumber As Integer
+    Friend PlayerControl As AxAXVLC.AxVLCPlugin2
+    'Private VideoFile As System.IO.File
+
+    Private Sub RenameForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.PlayerControl = Me.PlayerPanel.Controls.OfType(Of AxVLCPlugin2)().First
+        For Each Player In Form_Main.TableLayoutPanel1.Controls.OfType(Of AxAXVLC.AxVLCPlugin2)()
+            If Player.Name = "AxVLCPlugin2" & Me.PlayerNumber Then
+                PlayerControl = Player
+                Exit For
+            End If
+        Next
+        Me.PlayerControl.playlist.pause()
+
+        Dim a As Integer = 0
+        For a = 0 To Me.clb_cats.Items.Count - 1
+            Me.clb_cats.SetItemChecked(a, False)
+        Next
+        Me.cb_rating.SelectedIndex = -1
+
+        Me.l_filename.Text = System.IO.Path.GetFileName(Form_Main.options_list(Me.PlayerNumber).lb_playlist.Text)
+        Me.tb_changeto.Text = Me.l_filename.Text
+        Dim VW_Loc As Point = Me.PlayerControl.Parent.Parent.Location
+        Dim Player_Loc As Point = Me.PlayerControl.Location
+        Dim Mod_Loc As Point = New Point((Me.PlayerControl.Width / 2) - (Me.Width / 2), (Me.PlayerControl.Height / 2) - (Me.Height / 2))
+        Me.Location = VW_Loc + Player_Loc + Mod_Loc
+    End Sub
+
+    '>> Clicks
+
+    Private Sub b_rename_Click(sender As Object, e As EventArgs) Handles b_rename.Click
+
+        Dim invalids() As Char = IO.Path.GetInvalidFileNameChars
+        Dim fn As String = Me.tb_changeto.Text
+        Dim a As Integer = 0
+        While a < fn.Length
+            If invalids.Contains(fn(a)) Then
+                fn.Remove(a, 1)
+            Else
+                a = a + 1
+            End If
+        End While
+        Me.tb_changeto.Text = fn
+
+        Dim File As String = Form_Main.options_list(Me.PlayerNumber).lb_playlist.Text
+        Try
+            Form_Main.options_list(Me.PlayerNumber).b_remove_Click(Nothing, Nothing)
+            FileSystem.Rename(File, System.IO.Path.GetDirectoryName(File) & "\" & Me.tb_changeto.Text)
+        Catch ex As Exception
+            MsgBox("Error renaming file")
+        End Try
+        Me.Close()
+    End Sub
+
+    Private Sub b_cancel_Click(sender As Object, e As EventArgs) Handles b_cancel.Click
+        Me.PlayerControl.playlist.play()
+        Me.Close()
+    End Sub
+
+    Private Sub cb_rating_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb_rating.SelectedIndexChanged
+        Me.UpdateName()
+    End Sub
+
+    Private Sub clb_cats_SelectedIndexChanged(sender As Object, e As EventArgs) Handles clb_cats.SelectedIndexChanged
+        Me.UpdateName()
+    End Sub
+
+    Private Sub UpdateName()
+        Dim ChangeTo As String = " "
+        Dim orig As String = Me.l_filename.Text
+        For Each rating As String In Me.cb_rating.Items
+            If orig.StartsWith(rating & " ") Then
+                orig = orig.Substring(rating.Length + 1)
+                Exit For
+            End If
+        Next
+        If Me.cb_rating.SelectedItem <> "" Then
+            ChangeTo = Me.cb_rating.SelectedItem & " "
+            orig = orig.ToLower.Replace(Me.cb_rating.SelectedItem, "")
+            orig = orig.Replace("  ", " ")
+        End If
+
+        For Each cat As String In Me.clb_cats.CheckedItems
+            ChangeTo &= cat & " "
+            orig = orig.ToLower.Replace(cat, "")
+            orig = orig.Replace("  ", " ")
+        Next
+        orig = orig.Replace("  ", " ")
+        If ChangeTo.Length > 0 Then
+            ChangeTo &= orig
+        Else
+            ChangeTo = orig
+        End If
+        Me.tb_changeto.Text = ChangeTo
+        Me.tb_changeto.Text = Me.tb_changeto.Text.Replace("  ", " ").Trim
+    End Sub
+
+    Private Sub b_delete_Click(sender As Object, e As EventArgs) Handles b_delete.Click
+        Form_Main.options_list(Me.PlayerNumber).b_delete_Click(Nothing, Nothing)
+        Me.Close()
+    End Sub
+
+    Private Sub tb_changeto_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tb_changeto.KeyPress
+        If e.KeyChar = vbCr Then
+            Me.b_rename.PerformClick()
+        End If
+    End Sub
+End Class
